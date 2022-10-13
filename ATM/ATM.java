b80a83df42206b787697d7e80f51bca75968863d
Thiru0502 <114094260+Thiru0502@users.noreply.github.com>
2022-10-13 23:14:33 +0530

    Add files via upload

---
+++ ATM/ATM.java
@@ -0,0 +1,6 @@
+public class ATM extends Options{
+    public static void main(String[] args)  {
+      Options op=new Options();
+      op.login();
+    }
+}
---
+++ ATM/Options.java
@@ -0,0 +1,6 @@
+public class Options {
+  void login(){
+    System.out.println();
+    System.out.println("welcome to login page");
+  }  
+}
