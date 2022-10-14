60ac1cbf8693379bafeeaca0bc4e6623e6e05042
Thiru0502 <114094260+Thiru0502@users.noreply.github.com>
2022-10-14 22:09:05 +0530

    Add files via upload

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
+
 public class ATM extends Options{
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws IOException  {
       Options op=new Options();
       op.login();
     }
--- ATM/Options.java
+++ ATM/Options.java
@@ -1,6 +1,29 @@
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Options {
-  void login(){
+  Scanner menuInput=new Scanner(System.in);
+  DecimalFormat moneyFormat=new DecimalFormat("'$'###,##0.00");
+  HashMap<Integer,Integer> data=new HashMap<Integer,Integer>();
+
+  void login() throws IOException{
+    int x=1;
+    {
+      do{
+      try{
+        data.put(986677, 7868);
+        data.put(897867, 7678);
     System.out.println();
     System.out.println("welcome to login page");
+    System.out.println("Enter Your Account Number:");
+    
+  }catch(Exception e){
+  System.out.println("invalid characters(s).only numbers.");
+  x=2;
+  }
+}while(x==1);
+}
   }  
 }
